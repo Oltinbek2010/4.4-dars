01c94e390c426819b5576e91c15b2803814d6043
Oltinbek2010 <oltinbek04@gmail.com>
2024-06-06 16:26:03 +0500

    commit

--- js/index.js
+++ js/index.js
@@ -14,7 +14,7 @@
 //   }
 
 //   console.log(result);
- 
+
 // 2-masala
 // let a = +prompt(`String kiriting`);
 // let b = +prompt(`String kiriting`);
@@ -170,6 +170,215 @@
 
 // 4-masala
 
-let a = +prompt(`Sonni kiriting`);
+// let a = +prompt(`Sonni kiriting`);
 
-a % 2 == 1 ? console.log(`Toq`) : console.log(`Juft`);
+// a % 2 == 1 ? console.log(`Toq`) : console.log(`Juft`);
+
+// 5-masala
+
+// let a = +prompt(`Sonni kiriting`);
+
+// a > 0 ? console.log(`Musbat son`) : console.log(` Nol yoki manfiy son`);
+
+// Switch case
+
+// 1-masala
+
+// let a = +prompt(`Hafta kunining raqamini kiriting`);
+// switch (a) {
+//     case 1:
+//         console.log(`Dushanba`);
+//         break;
+//     case 2:
+//         console.log(`Seshnba`);
+//         break;
+//     case 3:
+//         console.log(`Chorshanba`);
+//         break;
+//     case 4:
+//         console.log(`Payshanba`);
+//         break;
+//     case 5:
+//         console.log(`Juma`);
+//         break;
+//     case 6:
+//         console.log(`Shanba`);
+//         break;
+//     case 7:
+//         console.log(`Yakshanba`);
+//         break;
+//     default:
+//         console.log(`Bunday hafta kuni yo'q!`);
+//         break;
+// }
+
+// 2-masala
+// let a = +prompt(`Oy raqamini kiriting`);
+// switch (a) {
+//     case 1:
+//         console.log(`Yanvar`);
+//         break;
+//     case 2:
+//         console.log(`Fevral`);
+//         break;
+//     case 3:
+//         console.log(`Mart`);
+//         break;
+//     case 4:
+//         console.log(`Aprel`);
+//         break;
+//     case 5:
+//         console.log(`May`);
+//         break;
+//     case 6:
+//         console.log(`Iyun`);
+//         break;
+//     case 7:
+//         console.log(`Iyul`);
+//         break;
+//     case 8:
+//         console.log(`Avgust`);
+//         break;
+//     case 9:
+//         console.log(`Sentyabr`);
+//         break;
+//     case 10:
+//         console.log(`Oktyabr`);
+//         break;
+//     case 11:
+//         console.log(`Noyabr`);
+//         break;
+//     case 12:
+//         console.log(`Dekabr`);
+//         break;
+//     default:
+//         console.log(`Bunday oy mavjud emas!`);
+//         break;
+// }
+
+// 3-masala
+
+// let a = +prompt(`A-F gacha harf yozing va kayfiyatingizni bilib oling`);
+
+// switch (a) {
+//     case A:
+//         console.log(`A'lo`);
+//         break;
+//     case B:
+//         console.log(`Zo'r`);
+//         break;
+//     case C:
+//         console.log(`Yaxshi`);
+//         break;
+//     case D:
+//         console.log(`O'rtacha`);
+//         break;
+//     case E:
+//         console.log(`Yomon`);
+//         break;
+//     case F:
+//         console.log(`Juda yomon`);
+//         break;
+//     default:
+//         console.log(`Ko'rsatilgan harfni yozing`);
+//         break;
+// }
+
+// 4-masala
+
+// let a = +prompt(`Oy raqamini kiriting`)
+
+// switch (a) {
+//     case 12:
+//     case 1:
+//     case 2:
+//         console.log(`Qish`);
+//         break;
+//     case 3:
+//     case 4:
+//     case 5:
+//         console.log(`Bahor`);
+//         break;
+//     case 6:
+//     case 7:
+//     case 8:
+//         console.log(`Yoz`);
+//         break;
+//     case 9:
+//     case 10:
+//     case 11:
+//         console.log(`Kuz`);
+//         break;
+//     default:
+//         console.log(`Bunday oy yo'q!`);
+//         break;
+// }
+
+// 5-masala
+
+// let a = +prompt(`3gacha raqam kiriting`)
+
+// switch (a) {
+//     case 1:
+//         console.log(`Birinchi`);
+//         break;
+//     case 2:
+//         console.log(`Ikkinchi`);
+//         break;
+//     case 3:
+//         console.log(`Uchinchi`);
+//         break;
+//     default:
+//         console.log(`No'malum qiymat`);
+//         break;
+// }
+
+// FOR
+
+// 1-masala
+
+// for (let i = 1; i < 11; i++) {
+//     console.log(i**2); 
+// }
+
+// 2-masala
+
+// for (let i = 11; i < 0; i-=1) {
+//     console.log(i);
+// }
+
+// 3-masala
+
+// let a = +prompt(`Massiv kiriting`)
+
+// for (let i = 1; i < a+1; i++) {
+//     console.log(i);
+// }
+
+// 4-masala
+
+// for (let i = 0; i < 21; i++) {
+//     if (i % 2 == 0) {
+//         console.log(i);
+//     }
+// }
+
+// 5-masala
+// let a = +prompt(`Massivni kiriting`);
+// let counter = 0;
+// for (let i = 0; i < a-1; i-=1) {
+//      if (i<0) {
+//         counter++;
+//      }
+// }
+// console.log(counter);
+
+// 6-masala
+
+let a = +prompt(`Massivni kiriting`);
+let sum = 0;
+
+for (let i = 0; i < a; i++) {
+    sum= sum + i;    
+}
+console.log(sum);
