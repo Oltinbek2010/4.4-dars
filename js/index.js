ed87b30f8ce5049ff83fd4c4dd8fafa258cb80bd
Oltinbek2010 <oltinbek04@gmail.com>
2024-06-06 15:33:25 +0500

    commit

--- js/index.js
+++ js/index.js
@@ -1,3 +1,175 @@
-for (let i = 10; i <= 1; i--) {
-    console.log(i);
-}
+// IF
+
+//   1-masala
+//   let a = +prompt(`1-sonni kiriting`);
+//   let b = +prompt(`2-sonni kiriting`);
+//   let c = +prompt(`3-sonni kiriting`);
+
+//   let result;
+
+//   if (a > b || b > c && c > 0) {
+//         result = `Shart bajarildi`;
+//   }if (c == 0) {
+//     result = ``;
+//   }
+
+//   console.log(result);
+ 
+// 2-masala
+// let a = +prompt(`String kiriting`);
+// let b = +prompt(`String kiriting`);
+
+// let result;
+
+// if (a(length > 5, Includes(a)) || b(Includes(b))) {
+//     result = `Shart bajarildi`;
+// }
+// console.log(result);
+
+// 3-masala
+
+// let a = +prompt (`1-sonni kiriting`);
+// let b = +prompt (`2-sonni kiriting`);
+// let c = +prompt (`3-sonni kiriting`);
+// let d = +prompt (`4-sonni kiriting`);
+
+// let result;
+
+// if (a + b + c + d > 50 && a > 10 || b > 10 || c > 10 || d > 10) {
+//     result = `Yig'indi katta va hech bo'lmaganda biri katta`;
+// }
+
+// console.log(result);
+
+// 4-masala
+
+// let a = +prompt (`1-sonni kiriting`);
+// let b = +prompt (`2-sonni kiriting`);
+
+// let result;
+
+// if (a * b > 100 || a + b > 20 && a % 2 == 0) {
+//     result = `Shart bajarildi!`;
+// }
+
+// console.log(result);
+
+// 5-masala
+
+// let a = +prompt(`String kiriting`);
+// let n = (`Sonni kiriting`);
+// let c = a / 2;
+
+// let result;
+
+// if (a(length > c, Includes(z)) || n > 50) {
+//     result = `Shart bajarildi`;
+// }
+// console.log(result);
+
+// 6-masala
+
+// let a = +prompt (`1-sonni kiriting`);
+// let b = +prompt (`2-sonni kiriting`);
+
+// let result; 
+
+// if (a % b == 0 && b > 0 || b % 5 == 0) {
+//     result = `Shart bajarildi`;
+// }
+
+// console.log(result);
+
+// 7-masala
+
+// let a = +prompt (`1-sonni kiriting`);
+// let b = +prompt (`2-sonni kiriting`);
+// let c = +prompt (`3-sonni kiriting`);
+
+// let result;
+
+// if (a > 0 && b > 0 && c > 0 && a**2 > b || c % 2 == 1) {
+//     result = `Shart bajarildi`;
+// }
+
+// 8-masala
+
+// let a = +prompt(`String kiriting`);
+// let b = +prompt(`String kiriting`);
+// let c = +prompt(`Son kiriting`);
+
+// let result;
+
+// if (a (length > c) && b (Includes(hello)) || c % 2 == 0) {
+//     result = `Shart bajarildi`;
+// }
+
+// console.log(result);
+
+// 9-masala
+
+// let a = +prompt (`1-sonni kiriting`);
+// let b = +prompt (`2-sonni kiriting`);
+// let c = +prompt (`3-sonni kiriting`);
+
+// let max;
+// let min;
+
+// if (a > b){
+//      max = a;
+//      min = b;
+// }else{
+//      min = a;
+//      max = b;
+// }
+// if (min > c){
+// min = c;
+// }
+// if(max < c){
+//      max = c;
+// }
+// let result = a + b + c - (max + min);
+// if (result > 10 && a > b || c < 15) {
+//      result = `Shart bajarildi`;
+// }
+// console.log(result);
+
+// 10-masala
+
+// let a = +prompt(`String kiriting`);
+// let b = +prompt(`String kiriting`);
+
+// let c = a(length);
+// let d = b(length);
+// let result;
+
+// if (a(Includes(e), c > 8) || b(d < 5), Includes(y)) {
+//     result = `Shart bajarildi`;
+// }
+// console.log(result);
+
+// Ternary
+
+// 1-masala
+
+// let a = +prompt(`Sonni kiriting`);
+// a > 0 ? console.log(`Musbat`):console.log(`Manfiy`);
+
+// 2-masala
+
+// let a = +prompt(`1-sonni kiriting`);
+// let b = +prompt(`2-sonni kiriting`);
+
+// a > b ? console.log(`Birinchi son katta`):console.log(`Ikkinshi son katta`);
+
+// 3-masala
+
+// let a = +prompt(`Stirng kiriting`);
+
+// a > 5 ? console.log(`Uzun string`) : console.log(`Qisqa string`);
+
+// 4-masala
+
+let a = +prompt(`Sonni kiriting`);
+
+a % 2 == 1 ? console.log(`Toq`) : console.log(`Juft`);
